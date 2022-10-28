e0eb3070e5180735a2947d9103620c25ca1321b1
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-10-27 22:37:51 -0500

    arg parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#![allow(unused)]
+use clap::Parser;
+use std::io::{Write, stdout, stdin};
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
+
 fn main() {
     println!("Hello, world!");
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file..");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
+
+
 }
